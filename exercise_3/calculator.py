65a8e33e85ae3abb7732322cf124af3a2a51a9d2
kgalb01 <k_galb01@uni-muenster.de>
2024-04-23 09:33:56 +0200

    created calculator class and main function to run

--- exercise_3/calculator.py
+++ exercise_3/calculator.py
@@ -2,3 +2,18 @@
 # Exercise 3
 # Author: Jonas Starke, Kieran Galbraith
 # Date: 2024-23-04
+
+class Calculator:
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
+
+    def multiply(self, x, y):
+        return x * y
+
+    def divide(self, x, y):
+        if y == 0:
+            raise ValueError("Cannot divide by zero")
+        return x / y
