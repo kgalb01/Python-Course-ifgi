6766a972eee2b6e58102ec3697a2f64f46f12863
kgalb01 <k_galb01@uni-muenster.de>
2024-05-04 12:35:38 +0200

    solution for exercise 4, task 1: wikipedia

---
+++ exercise_4/exercise_4_1.py
@@ -0,0 +1,22 @@
+# Python in QGIS and ArcGIS
+# Exercise 4
+# Author: Jonas Starke, Kieran Galbraith
+# Date: 2024-04-05
+
+from qgis.PyQt.QtCore import QUrl
+from qgis.PyQt.QtWebKitWidgets import QWebView
+
+# Get the name of the district
+district_name = "[%Name%]"
+
+# Construct the URL for the Wikipedia page of the district
+wikipedia_url = "https://en.wikipedia.org/wiki/" + district_name
+
+# Create an instance of QWebView
+web_view = QWebView()
+
+# Load the Wikipedia page URL
+web_view.load(QUrl(wikipedia_url))
+
+# Show the QWebView window
+web_view.show()
